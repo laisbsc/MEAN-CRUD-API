a170df9f0b570954724a98ebd55fbc1c0c1e3d8f
Lais Carvalho <santanal@tcd.ie>
2020-04-24 18:12:20 +0100

    Delete db.config.js

--- BlackBooksbackend/config/db.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    //local connection > NoDB.
-    //url: 'mongodb://localhost:27017'
-
-    //MongoDB Atlas link
-    url: 'mongodb+srv://test:EVcAkR60I0kE93lU@cluster0-izwzo.mongodb.net/test?retryWrites=true&w=majority'
-    //REMEMBER TO MAKE THIS A VARIABLE! > hide password from there
-  };
